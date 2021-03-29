32f5803269c47f4b0d026cea66d743ea3e94a7a1
Matthew Bayles <mmbayles@gmail.com>
2021-03-29 10:53:57 -0500

    added chart and aggregation into app. Still need to implement them with actual data.

---
+++ grazescape/model_defintions/crop_yield.py
@@ -0,0 +1,26 @@
+from abc import ABC
+
+from grazescape.model_defintions.model_base import ModelBase
+from pyper import R
+from django.conf import settings
+import os
+import numpy as np
+
+class CropYield(ModelBase):
+    def __init__(self,request, model_type, file_name=None):
+        super().__init__(request, file_name)
+        self.model_file_path = os.path.join(self.model_file_path, self.model_name)
+        self.model_type = model_type
+
+    # overwriting abstract method
+    def write_model_input(self, input_raster_dic, model_layer):
+        with open(self.model_data_inputs_path, "w") as f:
+            for y in range(0, len(input_raster_dic[self.model_type])):
+                for x in range(0, len(input_raster_dic[self.model_type][0])):
+                    f.write(str(input_raster_dic[self.model_type][y][x]) + "\n")
+
+    def run_model(self):
+
+        return np.loadtxt(self.model_data_inputs_path)
+
+
