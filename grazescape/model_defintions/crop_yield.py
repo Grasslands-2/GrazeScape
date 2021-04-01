c24e1192c1a323b3f03cf5758530fb56edd58958
Matthew Bayles <mmbayles@gmail.com>
2021-03-31 23:39:40 -0500

    Bar chart now works with multiple fields

--- grazescape/model_defintions/crop_yield.py
+++ grazescape/model_defintions/crop_yield.py
@@ -10,7 +10,7 @@ class CropYield(ModelBase):
     def __init__(self,request, model_type, file_name=None):
         super().__init__(request, file_name)
         self.model_type = model_type
-        self.units = "tons/ac"
+        self.units = "Dry Mass tons/ac"
 
     # overwriting abstract method
     def write_model_input(self, input_raster_dic, model_layer):
@@ -18,7 +18,7 @@ class CropYield(ModelBase):
             for y in range(0, len(input_raster_dic[self.model_type])):
                 for x in range(0, len(input_raster_dic[self.model_type][0])):
                     convert_value = input_raster_dic[self.model_type][y][x]
-                    convert_value = ((convert_value /10) * 56 / 2000)
+                    convert_value = ((convert_value / 10) * 56 / 2000)
                     f.write(str(convert_value) + "\n")
 
     def run_model(self):
