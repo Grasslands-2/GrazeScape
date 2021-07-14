672c940a35a8672a555c17423c4dabdc53872dcf
Zach Hassler <78423007+zhassler@users.noreply.github.com>
2021-07-14 14:09:30 -0500

    Update settings.py

--- grassland/settings.py
+++ grassland/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal301'
+#GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal301'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
