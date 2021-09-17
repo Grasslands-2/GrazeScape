93527c45aba3697307ed2150b5c8956f096c5bfb
zhassler <zhassler@wisc.edu>
2021-09-17 14:15:53 -0500

    infra profile is working! and its not to slow either some refinesments like turning off draw immidiately needed

--- grazescape/model_defintions/infra_profile_tool.py
+++ grazescape/model_defintions/infra_profile_tool.py
@@ -13,6 +13,7 @@ import geopandas as gpd
 from shapely.geometry import Polygon
 from django.conf import settings
 import math
+from itertools import cycle
 #from qgis import processing
 #from qgis.processing import alg
 #from qgis.processing import r
@@ -42,18 +43,27 @@ from pyper import R
 from django.conf import settings
 
 class InfraTrueLength():
-    def __init__(self,infraextent,infracords,infraId):
+    def __init__(self,infraextent,infracords,infraId,infraLengthXY):
         print('Hello from infra profile tool!')
-        print(infraId)
         #return('hi from python world')
-        self.extents = infraextent
-        self.infra_id = infraId
-        self.infracords = infracords
-        geo_server_url = "http://grazescape-dev1.glbrc.org:8080"
-
+        geo_server_url = settings.GEOSERVER_URL
         self.geoserver_url = geo_server_url + "/geoserver/ows?service=WCS&version=2.0.1&" \
-                             "request=GetCoverage&CoverageId="
-
+            "request=GetCoverage&CoverageId="
+        self.file_name = "infra_" + infraId
+        self.dir_path = os.path.join(settings.BASE_DIR, 'grazescape',
+            'data_files', 'raster_inputs',
+            self.file_name)
+        print(self.dir_path)
+        self.extents = infraextent
+        self.infra_id = str(infraId)
+        self.infraLengthXY = infraLengthXY
+        self.infracords = infracords
+        self.cordstring = ''
+        for i in self.infracords:
+            self.cordstring = self.cordstring + i + ','
+        self.cordstring = self.cordstring[:-1]
+        print('NEW COORDSTRING!!!!!!!!!!!!!!@!@$$$$$$$$$$$$$#@!!!!!!!!!!!!')
+        print(self.cordstring)
         self.extents_string_x = ""
         self.extents_string_y = ""
         print('infraextents!!!!!!!!!!!!!!!!!!!!!!!!!!!')
@@ -62,21 +72,40 @@ class InfraTrueLength():
             self.extents_string_x = "&subset=X(" + str(math.floor(float(infraextent[0]))) + "," + str(math.ceil(float(infraextent[2]))) + ")"
             self.extents_string_y = "&subset=Y(" + str(math.floor(float(infraextent[1]))) + "," + str(math.ceil(float(infraextent[3]))) + ")"
         self.crs = "epsg:3857"
+        self.no_data = -9999
         self.layer_dic = {
             "elevation": "InputRasters:TC_DEM"
         }
-        self.file_name = "infra_"+ infraId
-        self.dir_path = os.path.join(settings.BASE_DIR, 'grazescape',
-                                     'data_files', 'raster_inputs',
-                                     self.file_name)
+        self.bounds = {"x": 0, "y": 0}
+        self.no_data_aray = []
         # self.addOutput(QgsProcessingOutputFile(
         #     'NUMBEROFFEATURES',
         #         self.tr('Number of features processed')
         # ))
         # os.makedirs(self.dir_path)
+        os.makedirs(self.dir_path)
+        self.load_layersIP()
+        self.profileTool()
+    def load_layersIP(self):
+        """
+        Download data from geoserver
+        Returns
+        -------
 
-        self.load_dem()
-    def load_dem(self):
+        """
+        # layer_list = requests.get("http://localhost:8081/geoserver/rest/
+        # layers.json")
+
+        for layer in self.layer_dic:
+            print("downloading layer ", layer)
+            url = self.geoserver_url + self.layer_dic[layer] + self.extents_string_x + self.extents_string_y
+            r = requests.get(url)
+            raster_file_path = os.path.join(self.dir_path, layer + ".tif")
+            with open(raster_file_path, "wb") as f:
+                f.write(r.content)
+
+
+    def profileTool(self):
         """
         Download data from geoserver
         Returns
@@ -86,84 +115,34 @@ class InfraTrueLength():
         #processing.algorithmHelp("grass7:r.profile")
         output_file_path = os.path.join(self.dir_path, 'output.txt')
         print('RAN PROFILE TOOL!!!')
-        #Finally got the output to show up. It was working all along, it just didnt have an output folder to place the results in.  Work on getting that resolved with the DEM download, and folder creation.  Then work on disceting the output to pull out the data you need and do your trig!
-        
-        processing.run("grass7:r.profile", {'input':'C:/Users/zjhas/Documents/Ellisas_Rasters_08162021/DEM_10m_NED_3857_countuy_masked.tif','output':'C:/Users/zjhas/Documents/GrazeScape/grazescape/data_files/output3.txt','coordinates':'-10116500,5354750,-10116564,5355735,-10115331,5355468','resolution':None,'null_value':'*','file':'','-g':False,'-c':False,'GRASS_REGION_PARAMETER':None,'GRASS_REGION_CELLSIZE_PARAMETER':0})
-        
-        # layer_list = requests.get("http://localhost:8081/geoserver/rest/
-        # layers.json")
-        # for layer in self.layer_dic:
-        #     print("downloading layer ", layer)
-        #     url = self.geoserver_url + self.layer_dic[layer]
-        #     # + self.extents_string_x + self.extents_string_y
-        #     r = requests.get(url)
-        #     raster_file_path = os.path.join(self.dir_path, layer + ".tif")
-        #     output_file_path = os.path.join(self.dir_path, 'output.txt')
-        #     with open(raster_file_path, "wb") as f:
-        #         f.write(r.content)
-            
-        #     print(raster_file_path)
+       
+        processing.run("grass7:r.profile", {'input':self.dir_path + '/elevation.tif','output':self.dir_path + '/output3.txt','coordinates':self.cordstring,'resolution':None,'null_value':'*','file':'','-g':False,'-c':False,'GRASS_REGION_PARAMETER':None,'GRASS_REGION_CELLSIZE_PARAMETER':0})
 
-            # process = processing.run("grass7:r.profile", {'input':'C:/Users/zjhas/Documents/Ellisas_Rasters_08162021/DEM_10m_NED_3857_countuy_masked.tif','output':'C:/Users/zjhas/Documents/Ellisas_Rasters_08162021/profiletest5.txt','coordinates':'-10116500,5354750,-10116564,5355735,-10115331,5355468','resolution':None,'null_value':'*','file':'','-g':False,'-c':False,'GRASS_REGION_PARAMETER':None,'GRASS_REGION_CELLSIZE_PARAMETER':0})
-            # return(process)
-            
-            # return(processing.run("grass7:r.profile", {'input':raster_file_path,'coordinates':self.infracords,'resolution':0,'null_value':'-9999','file':'','-g':False,'-c':False,'output':output_file_path,'GRASS_REGION_PARAMETER':self.extents
-            # ,'GRASS_REGION_CELLSIZE_PARAMETER':1,'.complete_output':True}))
-
-            #print('RAN PROFILE TOOL!!!')
-
-
-        #self.geom = self.geom[0]
-    def create_clip(self, infraextent):
-        """
-        Create a shapefile to clip the raster with
-        Parameters
-        ----------
-        field_geom_array
-
-        Returns
-        -------
-
-        """
-        geom_array_float = []
-        for coor in infraextent:
-            geom_array_float.append([float(coor[0]), float(coor[1])])
-        polygon_geom = Polygon(geom_array_float)
-
-        crs = {'init': self.crs}
-        polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom])
-        polygon.to_file(filename=os.path.join(self.dir_path, self.file_name + ".shp"), driver="ESRI Shapefile")
-        return polygon.total_bounds
-
-    def clip_rasters(self):
-        """
-        Clip raster and return a rectangular array
- 
-        Returns array of values of the raster
-        -------
-
-        """
-        raster_data_dic = {}
-        bounds = 0
-        for file in os.listdir(self.dir_path):
-            if '.tif' in file:
-                data_name = file.split(".")[0]
-                image = gdal.Open(os.path.join(self.dir_path, file))
-                # set all output rasters to have float 32 data type
-                # this allows for the use of -9999 as no data value
-                # print("clipping raster ", data_name)
-                ds_clip = gdal.Warp(os.path.join(self.dir_path, data_name + "-clipped.tif"), image,
-                                    cutlineDSName=os.path.join(self.dir_path, self.file_name + ".shp"),
-                                    cropToCutline=True, dstNodata=self.no_data,outputType=gc.GDT_Float32)
-    
-    def calc(self):
-        #newID = float(self.infra_id) + 10
-        newID = self.infra_id
-        print('this is the geom obj')
-        print(newID)
-        #return(newID)
-
-        # grass.run_command('r.profile',
-        #        input = input_map,
-        #        output = output_file,
-        #        profile = [12244.256,-295112.597,12128.012,-295293.77])
+        f = open(self.dir_path + '/output3.txt', 'r')
+        content = f.read()
+        contentArray = content.splitlines()
+        profileValues = []
+        profileTotal = 0
+        totaltriside = 0
+        for i in contentArray:
+            lineoutput = i.split(' ')[2]
+            print(lineoutput)
+            if lineoutput != "*":
+                profileValues.append(float(lineoutput))
+        print(profileValues)
+        #valcycle = cycle(profileValues)
+        #nextelem = next(valcycle)
+        for index, elem in enumerate(profileValues):
+            if (index+1 < len(profileValues)):
+                curr_el = elem
+                next_el = profileValues[index + 1]
+                triside1 = abs(curr_el - next_el)
+                totaltriside = totaltriside + triside1
+                #hypoth = (math.sqrt(pow(triside1,2) + pow(10,2)) - 10) * 3.28084
+                print(triside1)
+                print(totaltriside)
+                #profileTotal = profileTotal + hypoth
+        profileTotal = math.sqrt(pow(totaltriside,2) + pow(float(self.infraLengthXY),2)) - float(self.infraLengthXY)
+        print(profileTotal)
+        profileTotalFT = profileTotal * 3.28084
+        return profileTotalFT
