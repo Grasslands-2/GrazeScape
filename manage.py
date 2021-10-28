d9c8f779ee90cb6a0506b64c0a141888e7dd342a
zhassler <zhassler@wisc.edu>
2021-10-28 13:30:42 -0500

    testing branch before merge to dev

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     # set DJANGO_SETTINGS_MODULE=grassland.settings.settings_prod
+    # set DJANGO_SETTINGS_MODULE=grassland.settings.settings_dev
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grassland.settings.settings_dev')
     try:
         from django.core.management import execute_from_command_line
