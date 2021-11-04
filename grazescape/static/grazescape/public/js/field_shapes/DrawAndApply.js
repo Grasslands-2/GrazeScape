509bbcf6fdb865060ab042f677b03260644c9dce
Zach Hassler <78423007+zhassler@users.noreply.github.com>
2021-11-04 10:59:32 -0500

    Update DrawAndApply.js

--- grazescape/static/grazescape/public/js/field_shapes/DrawAndApply.js
+++ grazescape/static/grazescape/public/js/field_shapes/DrawAndApply.js
@@ -69,6 +69,8 @@ async function createField(lac,non_lac,beef,crop,tillageInput,soil_pInput,field_
 	tillageDisp='';
 	grassDisp='';
 	grassVal='';
+	rotationFreqVal='';
+	rotationFreqdisp ='';
 	//--------------------Setting Display Values------------------
 	if(crop=='pt-cn'){
 		cropDisp ='Continuous Pasture';
