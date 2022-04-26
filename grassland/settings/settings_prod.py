3ac887a1ad38954d334b794ce503c969b4d5fb84
kleigeb <kevin.leigeb@gmail.com>
2022-04-26 17:02:55 -0500

    Update settings_prod.py
    fix the issues that caused grazescape-dev1 to barf when deploying the code

--- grassland/settings/settings_prod.py
+++ grassland/settings/settings_prod.py
@@ -17,11 +17,8 @@ if parser.has_section("captcha_google") and parser.has_section("postgresql"):
     params = parser.items("postgresql")
     db_name = params[1][1]
     db_user = params[2][1]
-    db_pass = os.environ['db_pass']
-    # Matt I switched this back last night to try to get the production container working.
+    db_pass = params[3][1]
     db_host = params[0][1]
-    print(os.environ)
-    print(db_pass)
 else:
     raise Exception(
         'Section {0} not found in the {1} file'.format("captcha_google", filename))
