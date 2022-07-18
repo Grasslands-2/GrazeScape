a3ced69062df56525621e1ced121a724d5853c57
zhassler <zhassler@wisc.edu>
2022-07-18 13:13:51 -0500

    turned off reset button in costs

--- grazescape/static/grazescape/public/js/state/NewScenario.js
+++ grazescape/static/grazescape/public/js/state/NewScenario.js
@@ -227,6 +227,8 @@ function createNewScenario(sname,sdescript){
 					alfalfa_pest_cost: 32,
 					alfalfa_mach_cost: 136.5,
 					alfalfa_mach_year_one: 225.50,
+					fert_n_cost: 1.0,
+					fert_p_cost: 1.0,
 				})
 				var geomType = 'point'
 				wfs_scenario_insert(newScenarioFeature, geomType,'scenarios_2')
