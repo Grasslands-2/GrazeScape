e193090ac4892b885c62a899d5b3b6cac381d6b7
Teagan Durtschi <teagan@durtschi.com>
2023-10-16 11:50:55 -0500

    Clean up region selector code, fixed a bug with border show/hide.

--- grazescape/static/grazescape/public/js/ExtUtils.js
+++ grazescape/static/grazescape/public/js/ExtUtils.js
@@ -65,7 +65,6 @@ Ext.define('DSS.utils', {
 						const region = farmRegion.getSource().getFeatures()[0];
 						const regionName = region.get("Name") || region.get("NAME");
 						farm.set("region", regionName);
-						console.log("Set region " + regionName + " for farm " + farm.get("farm_name"))
 					}
 				}
 			} catch (e) {
@@ -88,6 +87,142 @@ Ext.define('DSS.utils', {
 					}
                 }
             }
+		},
+
+		async selectRegion(region_name) {
+			let extent, view;
+			console.log("Selected Region: " + region_name);
+
+			if (region_name == 'CB_WI') {
+				selectedRegion = DSS.layer.cloverBeltBorder.getSource().getFeatures()[0];
+				DSS.activeRegion = "cloverBeltWI";
+				DSS.layer.cloverBeltBorder.setVisible(true)
+				DSS.layer.swwiBorder.setVisible(false)
+				DSS.layer.northeastBorder.setVisible(false)
+				DSS.layer.uplandBorder.setVisible(false)
+				DSS.layer.redCedarBorder.setVisible(false)
+				DSS.layer.pineRiverBorder.setVisible(false)
+
+				view = new ol.View({
+					center: [-10030031, 5610033],
+					zoom: 8,
+					maxZoom: 30,
+					minZoom: 8,
+					constrainOnlyCenter: false,
+					extent: [-10221386, 5467295, -9843661, 5750901]
+				})
+				extent = [-10221386, 5467295, -9843661, 5750901]
+			} else if (region_name == 'redCedarWI') {
+				selectedRegion = DSS.layer.redCedarBorder.getSource().getFeatures()[0];
+				DSS.activeRegion = "redCedarWI";
+
+				DSS.layer.redCedarBorder.setVisible(true)
+				DSS.layer.swwiBorder.setVisible(false)
+				DSS.layer.northeastBorder.setVisible(false)
+				DSS.layer.uplandBorder.setVisible(false)
+				DSS.layer.cloverBeltBorder.setVisible(false)
+				DSS.layer.pineRiverBorder.setVisible(false)
+
+				extent = [-10364871.915906506, 5523673.41766168, -10069499.539759004, 5831321.534259069]
+				view = new ol.View({
+					center: [-10217185.73, 5677497.476],
+					zoom: 8,
+					maxZoom: 30,
+					minZoom: 8,
+					constrainOnlyCenter: false,
+					extent: extent
+				})
+			} else if (region_name == 'pineRiverMN') {
+				selectedRegion = DSS.layer.pineRiverBorder.getSource().getFeatures()[0];
+				DSS.activeRegion = "pineRiverMN";
+
+				DSS.layer.pineRiverBorder.setVisible(true)
+				DSS.layer.redCedarBorder.setVisible(false)
+				DSS.layer.swwiBorder.setVisible(false)
+				DSS.layer.northeastBorder.setVisible(false)
+				DSS.layer.uplandBorder.setVisible(false)
+				DSS.layer.cloverBeltBorder.setVisible(false)
+
+				extent = [-10595719.33, 5805080.059, -10358718.96, 6020899.352]
+				view = new ol.View({
+					center: [-10458132.990856137, 5891167.715123995],
+					zoom: 8,
+					maxZoom: 30,
+					minZoom: 8,
+					constrainOnlyCenter: false,
+					extent: extent
+				})
+			} else if (region_name == 'SW_WI') {
+				selectedRegion = DSS.layer.swwiBorder.getSource().getFeatures()[0];
+				DSS.activeRegion = "southWestWI";
+
+				DSS.layer.cloverBeltBorder.setVisible(false)
+				DSS.layer.swwiBorder.setVisible(true)
+				DSS.layer.northeastBorder.setVisible(false)
+				DSS.layer.uplandBorder.setVisible(false)
+				DSS.layer.redCedarBorder.setVisible(false)
+				DSS.layer.pineRiverBorder.setVisible(false)
+
+				view = new ol.View({
+					center: [-10106698, 5391875],
+					zoom: 6,
+					maxZoom: 30,
+					minZoom: 6,
+					constrainOnlyCenter: false,
+					extent: [-10258162, 5258487, -9967076, 5520900]
+				})
+				extent = [-10258162, 5258487, -9967076, 5520900]
+			} else if (region_name == 'NE_WI') {
+				selectedRegion = DSS.layer.northeastBorder.getSource().getFeatures()[0];
+				DSS.activeRegion = "northeastWI";
+
+				DSS.layer.cloverBeltBorder.setVisible(false)
+				DSS.layer.swwiBorder.setVisible(false)
+				DSS.layer.northeastBorder.setVisible(true)
+				DSS.layer.uplandBorder.setVisible(false)
+				DSS.layer.redCedarBorder.setVisible(false)
+				DSS.layer.pineRiverBorder.setVisible(false)
+
+				view = new ol.View({
+					center: [-9786795, 5508847],
+					zoom: 6,
+					maxZoom: 30,
+					minZoom: 6,
+					constrainOnlyCenter: false,
+					extent: [-9861119, 5428671, -9706548, 5591254]
+				})
+				extent = [-9841119, 5448671, -9726548, 5571254]
+			} else if (region_name == 'UL_WI') {
+				selectedRegion = DSS.layer.uplandBorder.getSource().getFeatures()[0];
+				DSS.activeRegion = "uplandsWI";
+
+				DSS.layer.cloverBeltBorder.setVisible(false)
+				DSS.layer.swwiBorder.setVisible(false)
+				DSS.layer.northeastBorder.setVisible(false)
+				DSS.layer.uplandBorder.setVisible(true)
+				DSS.layer.redCedarBorder.setVisible(false)
+				DSS.layer.pineRiverBorder.setVisible(false)
+
+				view = new ol.View({
+					center: [-10039400, 5305041],
+					zoom: 6,
+					maxZoom: 30,
+					minZoom: 6,
+					constrainOnlyCenter: false,
+					extent: [-10247529, 5226215, -9938170, 5420242]
+				})
+				extent = [-10247529, 5226215, -9938170, 5420242]
+			} else {
+				throw new Error(`Error! Unknown region: ${region_name}`);
+			}
+
+			DSS.ApplicationFlow.instance.showFarmPickerPage();
+			DSS.map.un('pointermove', regionHighlighter);
+			DSS.map.removeInteraction(DSS.selectRP);
+			DSS.layer.farms_1.setVisible(true);
+			DSS.layer.regionLabels.setVisible(false);
+			await DSS.MapState.zoomToRealExtentRP(extent, view);
+			DSS.utils.filterFarmsLayerByRegion(selectedRegion);
 		},
 
 		filterFarmsLayerByRegion(selectedRegion) {			
--- grazescape/static/grazescape/public/js/map/RegionPickerIndicator.js
+++ grazescape/static/grazescape/public/js/map/RegionPickerIndicator.js
@@ -58,210 +58,7 @@ const selectStyle = new ol.style.Style({
 	})
 })
 
-function regionPickerFunc() {
-	console.log("INSIDE REGIONPICKERFUNC")
-	DSS.map.setView(new ol.View({
-		center: [-10090575.706307484, 5552204.392540871],
-		zoom: 7,
-		maxZoom: 30,
-		minZoom: 4,
-		constrainOnlyCenter: false,
-	}))
 
-	DSS.map.on('pointermove', regionHighlighter)
-	DSS.selectRP = new ol.interaction.Select({
-		features: new ol.Collection(),
-		toggleCondition: ol.events.condition.never,
-		layers: [DSS.layer.cloverBeltBorder,DSS.layer.swwiBorder,DSS.layer.northeastBorder,DSS.layer.uplandBorder,
-		DSS.layer.redCedarBorder,DSS.layer.pineRiverBorder],
-		style: new ol.style.Style({
-			stroke: new ol.style.Stroke({
-				color: 'rgba(255, 255, 255, 0.7)',
-				width: 4
-			}),
-			fill: new ol.style.Fill({
-				color: 'rgba(0,0,0,0)'
-			}),
-			zIndex: 5
-		})
-	});
-	DSS.map.addInteraction(DSS.selectRP);
-	console.log("select is on")
-	DSS.selectRP.on('select', async function(f) {
-		selectedRegion = f.selected[0]
-		let region_name = f.selected[0].get("Name") || f.selected[0].get("NAME");
-		console.log('select on', region_name);
-		DSS.map.un('pointermove', regionHighlighter)
-		DSS.layer.cloverBeltBorder.setStyle(unslectStyle)
-		DSS.layer.swwiBorder.setStyle(unslectStyle)
-		DSS.layer.uplandBorder.setStyle(unslectStyle)
-		DSS.layer.northeastBorder.setStyle(unslectStyle)
-		DSS.layer.redCedarBorder.setStyle(unslectStyle)
-		DSS.layer.pineRiverBorder.setStyle(unslectStyle)
-
-		AppEvents.triggerEvent('hide_region_picker_indicator')
-
-		DSS.utils.filterFarmsLayerByRegion(selectedRegion);
-
-		if(region_name == 'CB_WI'){
-			DSS.activeRegion = "cloverBeltWI";
-			AppEvents.triggerEvent('hide_region_picker_indicator')
-
-			DSS.layer.cloverBeltBorder.setVisible(true)
-			DSS.layer.swwiBorder.setVisible(false)
-			DSS.layer.northeastBorder.setVisible(false)
-			DSS.layer.uplandBorder.setVisible(false)
-            DSS.layer.redCedarBorder.setVisible(false)
-            DSS.layer.pineRiverBorder.setVisible(false)
-
-			DSS.layer.regionLabels.setVisible(false)
-			DSS.layer.farms_1.setVisible(true)
-			DSS.ApplicationFlow.instance.showFarmPickerPage();
-			DSS.map.removeInteraction(DSS.selectRP);
-			
-			let view = new ol.View({
-				center: [-10030031,5610033],
-				zoom: 8,
-				maxZoom: 30,
-				minZoom: 8,
-				constrainOnlyCenter: false,
-				extent:[-10221386, 5467295, -9843661, 5750901]
-			})
-			let extent = [-10221386, 5467295, -9843661, 5750901]
-			await DSS.MapState.zoomToRealExtentRP(extent,view)
-		}if(region_name == 'redCedarWI'){
-		    console.log("selected barron county!!!!!!!!!!!!!!!!!!!!!!!")
-			DSS.activeRegion = "redCedarWI";
-			AppEvents.triggerEvent('hide_region_picker_indicator')
-
-			DSS.layer.redCedarBorder.setVisible(true)
-			DSS.layer.swwiBorder.setVisible(false)
-			DSS.layer.northeastBorder.setVisible(false)
-			DSS.layer.uplandBorder.setVisible(false)
-            DSS.layer.cloverBeltBorder.setVisible(false)
-            DSS.layer.pineRiverBorder.setVisible(false)
-
-
-			DSS.layer.regionLabels.setVisible(false)
-			DSS.layer.farms_1.setVisible(true)
-			DSS.ApplicationFlow.instance.showFarmPickerPage();
-			DSS.map.removeInteraction(DSS.selectRP);
-            let extent = [-10364871.915906506, 5523673.41766168, -10069499.539759004,5831321.534259069]
-			let view = new ol.View({
-				center: [-10217185.73, 5677497.476],
-				zoom: 8,
-				maxZoom: 30,
-				minZoom: 8,
-				constrainOnlyCenter: false,
-				extent:extent
-			})
-			await DSS.MapState.zoomToRealExtentRP(extent,view)
-		}if(region_name == 'pineRiverMN'){
-		    console.log("pine")
-			DSS.activeRegion = "pineRiverMN";
-			AppEvents.triggerEvent('hide_region_picker_indicator')
-
-			DSS.layer.pineRiverBorder.setVisible(true)
-			DSS.layer.redCedarBorder.setVisible(false)
-			DSS.layer.swwiBorder.setVisible(false)
-			DSS.layer.northeastBorder.setVisible(false)
-			DSS.layer.uplandBorder.setVisible(false)
-            DSS.layer.cloverBeltBorder.setVisible(false)
-
-			DSS.layer.regionLabels.setVisible(false)
-			DSS.layer.farms_1.setVisible(true)
-			DSS.ApplicationFlow.instance.showFarmPickerPage();
-			DSS.map.removeInteraction(DSS.selectRP);
-            let extent = [ -10595719.33,	5805080.059,	-10358718.96,	6020899.352]
-			let view = new ol.View({
-				center: [-10458132.990856137, 5891167.715123995],
-				zoom: 8,
-				maxZoom: 30,
-				minZoom: 8,
-				constrainOnlyCenter: false,
-				extent:extent
-			})
-			await DSS.MapState.zoomToRealExtentRP(extent,view)
-		}if(region_name == 'SW_WI'){
-			DSS.activeRegion = "southWestWI";
-			AppEvents.triggerEvent('hide_region_picker_indicator')
-			
-			DSS.layer.cloverBeltBorder.setVisible(false)
-			DSS.layer.swwiBorder.setVisible(true)
-			DSS.layer.northeastBorder.setVisible(false)
-			DSS.layer.uplandBorder.setVisible(false)
-			DSS.layer.redCedarBorder.setVisible(true)
-			DSS.layer.pineRiverBorder.setVisible(false)
-			
-			DSS.layer.regionLabels.setVisible(false)
-			DSS.layer.farms_1.setVisible(true)
-			DSS.ApplicationFlow.instance.showFarmPickerPage();
-			DSS.map.removeInteraction(DSS.selectRP);
-			let view = new ol.View({
-				center: [-10106698,5391875],
-				zoom: 6,
-				maxZoom: 30,
-				minZoom: 6,
-				constrainOnlyCenter: false,
-				extent:[-10258162, 5258487, -9967076, 5520900]
-			})
-			let extent = [-10258162, 5258487, -9967076, 5520900]
-			await DSS.MapState.zoomToRealExtentRP(extent,view)
-		}if(region_name == 'NE_WI'){
-			DSS.activeRegion = "northeastWI";
-			AppEvents.triggerEvent('hide_region_picker_indicator')
-			
-			DSS.layer.cloverBeltBorder.setVisible(false)
-			DSS.layer.swwiBorder.setVisible(false)
-			DSS.layer.northeastBorder.setVisible(true)
-			DSS.layer.uplandBorder.setVisible(false)
-			DSS.layer.redCedarBorder.setVisible(true)
-			DSS.layer.pineRiverBorder.setVisible(false)
-			
-			DSS.layer.regionLabels.setVisible(false)
-			DSS.layer.farms_1.setVisible(true)
-			DSS.ApplicationFlow.instance.showFarmPickerPage();
-			DSS.map.removeInteraction(DSS.selectRP);
-			let view = new ol.View({
-				center: [-9786795,5508847],
-				zoom: 6,
-				maxZoom: 30,
-				minZoom: 6,
-				constrainOnlyCenter: false,
-				extent:[-9861119, 5428671, -9706548, 5591254]
-			})
-			let extent = [-9841119, 5448671, -9726548, 5571254]
-			await DSS.MapState.zoomToRealExtentRP(extent,view)
-		}
-		if(region_name == 'UL_WI'){
-			DSS.activeRegion = "uplandsWI";
-			AppEvents.triggerEvent('hide_region_picker_indicator')
-			
-			DSS.layer.cloverBeltBorder.setVisible(false)
-			DSS.layer.swwiBorder.setVisible(false)
-			DSS.layer.northeastBorder.setVisible(false)
-			DSS.layer.uplandBorder.setVisible(true)
-			DSS.layer.redCedarBorder.setVisible(true)
-			DSS.layer.pineRiverBorder.setVisible(false)
-			
-			DSS.layer.regionLabels.setVisible(false)
-			DSS.layer.farms_1.setVisible(true)
-			DSS.ApplicationFlow.instance.showFarmPickerPage();
-			DSS.map.removeInteraction(DSS.selectRP);
-			let view = new ol.View({
-				center: [-10039400,5305041],
-				zoom: 6,
-				maxZoom: 30,
-				minZoom: 6,
-				constrainOnlyCenter: false,
-				extent:[-10247529, 5226215, -9938170, 5420242]
-			})
-			let extent = [-10247529, 5226215, -9938170, 5420242]
-			await DSS.MapState.zoomToRealExtentRP(extent,view)
-		}
-		DSS.map.removeInteraction(DSS.selectRP);
-	})
-}
 
 //------------------------------------------------------------------------------
 Ext.define('DSS.map.RegionPickerIndicator', {
@@ -293,23 +90,14 @@ Ext.define('DSS.map.RegionPickerIndicator', {
 				allowDepress: false,
 				frame: false
 			},
-			items: [
-			// 	{
-			// 	//xtype: 'component',
-			// 	text: 'Pick a Region to Work in! ',
-			// 	//tooltip: 'Draw fieldstructure lines',
-			// 	width: 200
-			// }
-		]
+			items: []
 		});
 		
 		me.callParent(arguments);
 		
-		//me.showAt(400, -38); me.setHidden(true);
-		
 		AppEvents.registerListener('show_region_picker_indicator', function() {
 			let om = Ext.getCmp('ol_map');
-			let x = om.getX() + (om.getWidth() - /*me.getWidth()*/258) * 0.5;
+			let x = om.getX() + (om.getWidth() - 258) * 0.5;
 			me.setHidden(false);
 			me.setX(x);
 			me.stopAnimation().animate({
@@ -319,17 +107,7 @@ Ext.define('DSS.map.RegionPickerIndicator', {
 				}
 			})
 		})
-		AppEvents.registerListener('hide_region_picker_indicator', function() {
-			me.stopAnimation().animate({
-				duration: 300,
-				to: {
-					y: -38
-				},
-				callback: function() {
-					me.setHidden(true);
-				}
-			})
-		})
+
 		AppEvents.registerListener('map_resize', function() {
 			if (!me.isHidden()) {
 				let om = Ext.getCmp('ol_map');
