0afb56e33ceaa27e6b3113a17da36ca32ccf4a67
Matthew Bayles <mmbayles@gmail.com>
2024-03-17 13:09:40 -0500

    Updated incorrect file paths

--- floodscape/floodscape_models/WorkflowTrigger.py
+++ floodscape/floodscape_models/WorkflowTrigger.py
@@ -12,13 +12,24 @@ from django.conf import settings
 
 
 def hms_trigger(cn_dict_model, cn_dict_base, watershed):
+    command_path = os.path.join(settings.BASE_DIR, 'floodscape', 'floodscape_models')
     if watershed == "COON CREEK Main":
         project_dir = os.path.join(settings.HMS_MODEL_PATH, "HMS_CC_Final")
-        command = "hec-hms.exe -s M://Work/GrazeScape/floodscape/floodscape_models/cc.script"
+        if settings.HMS_MODEL_PATH == "/tmp/floodScape":
+            script_name = "cc_prod.script"
+        else:
+            script_name = "cc.script"
+        command_path = os.path.join(command_path, script_name)
+        command = "hec-hms.exe -s " + command_path
         cn_adjust.prepare_model_runs(project_dir, cn_dict_model, cn_dict_base)
     elif watershed == "West Fork Kickapoo Main":
         project_dir = os.path.join(settings.HMS_MODEL_PATH, "HMS_WFK_Final")
-        command = "hec-hms.exe -s M://Work/GrazeScape/floodscape/floodscape_models/wf.script"
+        if settings.HMS_MODEL_PATH == "/tmp/floodScape":
+            script_name = "wf_prod.script"
+        else:
+            script_name = "wf.script"
+        command_path = os.path.join(command_path, script_name)
+        command = "hec-hms.exe -s " + command_path
         wfk_cn_adjust.prepare_model_runs(project_dir, cn_dict_model, cn_dict_base)
     else:
         raise ValueError("watershed region is not correct")
@@ -33,24 +44,27 @@ def hms_trigger(cn_dict_model, cn_dict_base, watershed):
     # Run the command and capture the output-
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True)
+        output = subprocess.check_output(command, shell=True, universal_newlines=True, stderr=subprocess.STDOUT)
+
         print("HEC-HMS execution successful.")
         print("Output:")
         print(result.stdout)
+        print(output)
     except subprocess.CalledProcessError as e:
         print(f"HEC-HMS execution failed with error: {e}")
         print("Error output:")
         print(e.stderr)
     #
-    if watershed == "COON CREEK Main":
-        output = DSSOutput(project_dir)
-        output.run()
-        model_file_path = compile_data.compile_data_to_json(project_dir)
-        base_file_path = os.path.join(project_dir, "BaseLineStorms","CompiledRiverStationData_Base.json")
-    elif watershed == "West Fork Kickapoo Main":
-        # output = WFKDSSOutput(project_dir)
-        # output.run()
-        model_file_path = wfk_compile_data.compile_data_to_json(project_dir)
-        base_file_path = os.path.join(project_dir, "BaseLineStorms","CompiledRiverStationData_Base.json")
+    # if watershed == "COON CREEK Main":
+    #     output = DSSOutput(project_dir)
+    #     output.run()
+    #     model_file_path = compile_data.compile_data_to_json(project_dir)
+    #     base_file_path = os.path.join(project_dir, "BaseLineStorms","CompiledRiverStationData_Base.json")
+    # elif watershed == "West Fork Kickapoo Main":
+    #     # output = WFKDSSOutput(project_dir)
+    #     # output.run()
+    #     model_file_path = wfk_compile_data.compile_data_to_json(project_dir)
+    #     base_file_path = os.path.join(project_dir, "BaseLineStorms","CompiledRiverStationData_Base.json")
     os.chdir(cur_dir)
     return model_file_path, base_file_path
 
